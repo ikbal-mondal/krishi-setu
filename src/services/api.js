6d5cf3d41c030e5170d1a9283b736bf9777c761a
Your Name <your-email@example.com>
2025-11-09 13:47:55 +0530

    just add url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { getAuth } from "firebase/auth";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://krishisetuserser.vercel.app/api",
+  // import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 // attach Firebase token (if logged in)
